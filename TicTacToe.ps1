46395cb23b7196f2b1562f94e62a9bbccafa9bf2
Ted Ekström <80516576+TedEkstrom@users.noreply.github.com>
2023-03-12 13:24:19 +0100

    Update TicTacToe.ps1

--- TicTacToe.ps1
+++ TicTacToe.ps1
@@ -1,4 +1,4 @@
-﻿
+
 $borad = @(
 
     "[ ]", "[ ]", "[ ]",
@@ -61,79 +61,136 @@ $art4 = "
                            ..   .. 
 "
 
-Write-Host $art
+function scoreboard ( $P=0, $A=0 ) {
+   
+   $Pp = ""
+   $Ap = ""
 
-$AI = ""
+   if ( $P -lt 10 ) {
+         
+        $Pp = "       "
+        $Ap = "       "
+   } else {
 
-while ( $true ) {
-    $Player = (Read-Host "Choise a player (X or O)").ToUpper()
-    if ( $Player -eq "X" ) {
-        $AI = "O"
-        break
-    } elseif ( $Player -eq "O" ) {
-        $AI = "X"
-        break
-    }
-}
+        $Pp = "      "
+        $Ap = "      "
+   }
 
-while ( $true ) {
-    cls
+   Write-Host "
+               
+               ########################################
+               #                                      #
+               #           Game score                 #
+               #           Player: $($P)$Pp           #
+               #           AI:     $($A)$Ap           #
+               #                                      #
+               ########################################
+              
+              "
+} 
+
+$Continue = $true
+$AIScore = 0
+$PlayerScore = 0
+
+:game while ( $Continue ) {
+   
     Write-Host $art
 
-    "X", "O" | ForEach-Object {
-        if ( (($borad[0] -eq "[$_]") -and ($borad[1] -eq "[$_]") -and ($borad[2] -eq "[$_]")) -or `
-             (($borad[3] -eq "[$_]") -and ($borad[4] -eq "[$_]") -and ($borad[5] -eq "[$_]")) -or `
-             (($borad[6] -eq "[$_]") -and ($borad[7] -eq "[$_]") -and ($borad[8] -eq "[$_]")) -or `
+    $AI = ""
+    $Player = ""
 
-             (($borad[0] -eq "[$_]") -and ($borad[3] -eq "[$_]") -and ($borad[6] -eq "[$_]")) -or `
-             (($borad[1] -eq "[$_]") -and ($borad[4] -eq "[$_]") -and ($borad[7] -eq "[$_]")) -or `
-             (($borad[2] -eq "[$_]") -and ($borad[5] -eq "[$_]") -and ($borad[8] -eq "[$_]")) ) {
-
-            if ( $_ -eq "$Player" ) {
-                Write-Host $art2
-                break
-            } else {
-                Write-Host $art3
-                break
-            }
-        }
-    }
-
-    if ( $borad -notcontains "[ ]" ) {
-        
-        Write-Host $art4
-        break
-    }
-    
-    Write-Host "-------------------"
-    Write-Host "| $($borad[0]) | $($borad[1]) | $($borad[2]) |"
-    Write-Host "| $($borad[3]) | $($borad[4]) | $($borad[5]) |"
-    Write-Host "| $($borad[6]) | $($borad[7]) | $($borad[8]) |"
-    Write-Host "-------------------"
-    
-    $Move = Read-Host "Enter a move (1 - 9)"
-    while ( $borad[$Move-1] -ne "[ ]" ) {
-        $Move = Read-Host "Enter a move (1 - 9)"
-    }
-
-    $borad[$Move-1] = "[$Player]"
-
-    $AI_Move = Get-Random -Maximum 8
-    $Count = 0
-    while ( $borad[$AI_Move] -ne "[ ]" ) {
-        $AI_Move = Get-Random -Maximum 8
-        if ( $Count -eq 10 ) {
-            $AI_Move = $null
+    while ( $true ) {
+        $Player = (Read-Host "Choise a player (X or O)").ToUpper()
+        if ( $Player -eq "X" ) {
+            $AI = "O"
+            break
+        } elseif ( $Player -eq "O" ) {
+            $AI = "X"
             break
         }
-        $Count++
     }
 
-    if ( $AI_Move -ne $null) {
-        $borad[$AI_Move] = "[$AI]"
+    :gameplay while ( $true ) {
+        cls
+        Write-Host $art
+
+        "X", "O" | ForEach-Object {
+            if ( (($borad[0] -eq "[$_]") -and ($borad[1] -eq "[$_]") -and ($borad[2] -eq "[$_]")) -or `
+                 (($borad[3] -eq "[$_]") -and ($borad[4] -eq "[$_]") -and ($borad[5] -eq "[$_]")) -or `
+                 (($borad[6] -eq "[$_]") -and ($borad[7] -eq "[$_]") -and ($borad[8] -eq "[$_]")) -or `
+
+                 (($borad[0] -eq "[$_]") -and ($borad[3] -eq "[$_]") -and ($borad[6] -eq "[$_]")) -or `
+                 (($borad[1] -eq "[$_]") -and ($borad[4] -eq "[$_]") -and ($borad[7] -eq "[$_]")) -or `
+                 (($borad[2] -eq "[$_]") -and ($borad[5] -eq "[$_]") -and ($borad[8] -eq "[$_]")) ) {
+
+                if ( $_ -eq "$Player" ) {
+                    Write-Host $art2
+                    $PlayerScore++
+                    
+                } else {
+                    Write-Host $art3
+                    $AIScore++
+                    
+                }
+                while ( $true ) {
+                    $ContinuePlaying = (Read-Host "Continue playing? (Yes/No)").ToUpper()
+                    if ( $ContinuePlaying -eq "YES" ) {
+                        $Continue = $true
+                        break gameplay
+                    } elseif ( $ContinuePlaying -eq "NO" ) {
+                        $Continue = $false
+                        scoreboard -PlayerScore $PlayerScore -AIScore $AIScore
+                        break gameplay
+                    }
+                }
+            }
+        }
+
+        if ( $borad -notcontains "[ ]" ) {
+        
+            Write-Host $art4
+            $AIScore++
+            $PlayerScore++
+
+            while ( $true ) {
+                $ContinuePlaying = (Read-Host "Continue playing? (Yes/No)").ToUpper()
+                if ( $ContinuePlaying -eq "YES" ) {
+                    $Continue = $true
+                    break gameplay
+                } elseif ( $ContinuePlaying -eq "NO" ) {
+                    $Continue = $false
+                    break gameplay
+                }
+            }
+        }
+    
+        Write-Host "-------------------"
+        Write-Host "| $($borad[0]) | $($borad[1]) | $($borad[2]) |"
+        Write-Host "| $($borad[3]) | $($borad[4]) | $($borad[5]) |"
+        Write-Host "| $($borad[6]) | $($borad[7]) | $($borad[8]) |"
+        Write-Host "-------------------"
+    
+        $Move = Read-Host "Enter a move (1 - 9)"
+        while ( $borad[$Move-1] -ne "[ ]" ) {
+            $Move = Read-Host "Enter a move (1 - 9)"
+        }
+
+        $borad[$Move-1] = "[$Player]"
+
+        $AI_Move = Get-Random -Maximum 8
+        $Count = 0
+        while ( $borad[$AI_Move] -ne "[ ]" ) {
+            $AI_Move = Get-Random -Maximum 8
+            if ( $Count -eq 10 ) {
+                $AI_Move = $null
+                break
+            }
+            $Count++
+        }
+
+        if ( $AI_Move -ne $null) {
+            $borad[$AI_Move] = "[$AI]"
+        }
     }
 }
-
-
-
-
